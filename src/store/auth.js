79f8daab3023550de0a249ae5e92ee3dec9e0701
Roman <67958372+TRK08@users.noreply.github.com>
2021-07-12 15:02:05 +0300

    add some css styles

--- src/store/auth.js
+++ src/store/auth.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 const auth = {
   namespaced: true,
-  state : {
+  state: {
     token: null,
     user: null,
     error: false,
     preload: false
   },
   mutations: {
-		SET_USER(state, user){
-			state.user = user
-		},
-		SET_TOKEN(state, token) {
+    SET_USER(state, user) {
+      state.user = user
+    },
+    SET_TOKEN(state, token) {
       state.token = token;
     },
     SET_ERROR(state, err) {
@@ -21,16 +21,17 @@ const auth = {
     PRELOADER(state) {
       state.preload = !state.preload
     },
-    CHANGE_AVATAR(state, avatar){
+    CHANGE_AVATAR(state, avatar) {
       state.user.avatar = avatar
       localStorage.setItem("user", JSON.stringify(state.user));
-  }
+    }
   },
   actions: {
-    SET_PRELOAD ({commit}) {
+    SET_PRELOAD({ commit }) {
       commit('PRELOADER')
     },
-    async REGISTR({commit}, user) {
+
+    async REGISTR({ commit }, user) {
       try {
         let requestParams = {
           name: user.name,
@@ -40,59 +41,59 @@ const auth = {
         }
 
         await axios
-        .post(`https://pomogayka96.ru/wp-json/pg/v1/registration?`, user, {params: requestParams})
-        .then(res => {
-        })
+          .post(`https://pomogayka96.ru/wp-json/pg/v1/registration?`, user, { params: requestParams })
+          .then(res => {
+          })
       }
       catch (err) {
         alert('Произошла ошибка, попробуйте позже')
       }
     },
 
-    async AUTH_REQUEST({commit, dispatch }, payload) {
+    async AUTH_REQUEST({ commit, dispatch }, payload) {
       try {
-          const {data} = await axios.post(`https://pomogayka96.ru/wp-json/jwt-auth/v1/token`, payload)
-          let error = false
-          commit('SET_ERROR', error)
-          return dispatch('VALIDATE', data)
+        const { data } = await axios.post(`https://pomogayka96.ru/wp-json/jwt-auth/v1/token`, payload)
+        let error = false
+        commit('SET_ERROR', error)
+        return dispatch('VALIDATE', data)
       }
       catch (err) {
-        if(err.message.slice(-3) === '403') {
+        if (err.message.slice(-3) === '403') {
           let error = true
           commit('SET_ERROR', error)
         }
         else {
           alert('Что-то пошло не так')
         }
-        
+
       }
     },
     async VALIDATE({ commit, state }, user) {
       if (user) {
-          commit("SET_TOKEN", user.token);
-          commit("SET_USER", user);
+        commit("SET_TOKEN", user.token);
+        commit("SET_USER", user);
       }
       if (!state.user) {
-          return
+        return
       }
       try {
-          const response = await axios({
-              url: 'https://pomogayka96.ru/wp-json/jwt-auth/v1/token/validate',
-              method: 'post',
-              headers: {
-                  'Authorization': `Bearer ${user.token}`
-              }
-          });
-          let error = false
-          commit('SET_ERROR', error)
-          localStorage.setItem("user", JSON.stringify(user));
-          commit("SET_TOKEN", user.token);
-          commit("SET_USER", user);
+        const response = await axios({
+          url: 'https://pomogayka96.ru/wp-json/jwt-auth/v1/token/validate',
+          method: 'post',
+          headers: {
+            'Authorization': `Bearer ${user.token}`
+          }
+        });
+        let error = false
+        commit('SET_ERROR', error)
+        localStorage.setItem("user", JSON.stringify(user));
+        commit("SET_TOKEN", user.token);
+        commit("SET_USER", user);
       }
       catch (err) {
-          localStorage.removeItem("user");
-          commit("SET_TOKEN", null);
-          commit("SET_USER", null);
+        localStorage.removeItem("user");
+        commit("SET_TOKEN", null);
+        commit("SET_USER", null);
       }
     },
     async SIGN_OUT({ commit }) {
@@ -100,14 +101,14 @@ const auth = {
       commit("SET_TOKEN", null);
       commit("SET_USER", null);
     },
-    changeAvatar({commit}, avatar){
+    changeAvatar({ commit }, avatar) {
       console.log(avatar);
       localStorage.removeItem("user");
       commit("CHANGE_AVATAR", avatar)
-  }
+    }
   },
   getters: {
-    getAuthenticated(state){
+    getAuthenticated(state) {
       return state.user
     },
     getError(state) {
